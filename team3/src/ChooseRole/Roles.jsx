1974543bdac396f278a1b451aac198e75fef9069
leoqode <angel.jimenez2@g.austincc.edu>
2024-11-01 19:21:24 -0400

    Tailwind fixes

---
+++ team3/src/ChooseRole/Roles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Roles = () => {
+  return (
+    <div className='color-red'>
+      <button>Teacher</button>
+      <button>Proprietor</button>
+    </div>
+  )
+}
+
+export default Roles
