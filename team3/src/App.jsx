788563640da81e65ff457941e59a6e61c6057898
leoqode <angel.jimenez2@g.austincc.edu>
2024-11-01 20:05:19 -0400

    buttons styling

--- team3/src/App.jsx
+++ team3/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Roles from './ChooseRole/Roles'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
+      <Roles />
     </>
   )
 }
--- team3/src/ChooseRole/Roles.jsx
+++ team3/src/ChooseRole/Roles.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Roles = () => {
   return (
-    <div className='color-red'>
-      <button>Teacher</button>
-      <button>Proprietor</button>
+    <div className='flex flex-col items-center h-full justify-center'>
+      <button className='bg-gradient-to-r from-orange-400 via-pink-600 to-teal-400 text-white px-8 py-3 rounded-lg hover:from-orange-600 hover:to-pink-600 transition-all transform hover:scale-105 mt-4'>Teacher</button>
+      <button className='bg-gradient-to-r from-orange-400 via-pink-600 to-teal-400 text-white px-8 py-3 rounded-lg hover:from-orange-600 hover:to-pink-600 transition-all transform hover:scale-105 mt-4'>Proprietor</button>
     </div>
   )
 }
