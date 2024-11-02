2d74106c1cff2a74bec026411e183502f23e9a4b
hwang199 <helenwang162@gmail.com>
2024-11-01 21:14:21 -0400

    updated list of teachers with examples

--- team3/backend/server.js
+++ team3/backend/server.js
@@ -11,11 +11,11 @@ http.createServer(function (req, res) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end('<h1>Hello World!</h1>');
         } else if (parsedUrl.pathname === '/teachers') {
-            const list_of_teachers = {
-                fname: 'Jacqueline',
-                lname: 'Batshuayi',
-                subjects_taught:'Math, Science, History'
-            };
+            const list_of_teachers = [
+                {fname: 'Jacqueline', lname: 'Batshuayi', subjects_taught: 'Math, Science, History', grades_taught: '1st, 2nd, 3rd'},
+                {fname: 'Anika', lname: 'Bauma', subjects_taught: 'Language Arts, Science', grades_taught: '1st, 2nd'},
+                {fname: 'Allison', lname: 'Bokungu', subjects_taught: 'Math, Science, Language Arts', grades_taught: '1st, 2nd, 3rd, 4th'}
+            ];
             res.writeHead(200, {'Content-Type': 'application/json'});
             const jsonContent = JSON.stringify(responseData);
             res.end(jsonContent);
