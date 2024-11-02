63762b47a4f96f6fb6192e55385538f1257bfd61
hwang199 <helenwang162@gmail.com>
2024-11-01 20:26:57 -0400

    added json format response

--- team3/backend/server.js
+++ team3/backend/server.js
@@ -3,19 +3,24 @@ var url = require('url');
 
 http.createServer(function (req, res) {
   // this parses the URL
-  var parsedUrl = url.parse(req.url, true);
+    var parsedUrl = url.parse(req.url, true);
 
   // for get requests
-  if (req.method === 'GET') {
-    if (parsedUrl.pathname === '/') {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('<h1>Hello World!</h1>');
-    } else if (parsedUrl.pathname === '/about') {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('<h1>About Page</h1>');
-    } else {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      res.end('<h1>404 Not Found</h1>');
+    if (req.method === 'GET') {
+        if (parsedUrl.pathname === '/') {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>Hello World!</h1>');
+        } else if (parsedUrl.pathname === '/teachers') {
+            // Send JSON response
+            const responseData = {
+            message: 'Hello, world!',
+            timestamp: new Date()
+        };
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(responseData));
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end('<h1>404 Not Found</h1>');
     }
   }
 
