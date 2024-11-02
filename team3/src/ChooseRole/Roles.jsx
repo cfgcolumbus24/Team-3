5031ab7b8a7cfc6277721efd2aaff039afd83c30
rsmkhal <133159514+rsmkhal@users.noreply.github.com>
2024-11-02 03:02:32 -0400

    Updated Roles.jsx in ChooseRole directory with new changes

--- team3/src/ChooseRole/Roles.jsx
+++ team3/src/ChooseRole/Roles.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Roles = () => {
   return (
-    <div className='flex flex-col items-center h-full justify-center'>
-      <button className='bg-gradient-to-r from-orange-400 via-pink-600 to-teal-400 text-white px-8 py-3 rounded-lg hover:from-orange-600 hover:to-pink-600 transition-all transform hover:scale-105 mt-4'>Teacher</button>
-      <button className='bg-gradient-to-r from-orange-400 via-pink-600 to-teal-400 text-white px-8 py-3 rounded-lg hover:from-orange-600 hover:to-pink-600 transition-all transform hover:scale-105 mt-4'>Proprietor</button>
-    </div>
-  )
-}
+    <div className='relative min-h-screen'>
+      <img src="/opp_int.jpg" alt="Logo" className="absolute top-5 left-5 w-16 h-auto" />
 
-export default Roles
+      <div className='flex flex-col items-center justify-center min-h-screen'>
+        <h2 className='text-2xl font-semibold mb-8'>You are a:</h2>
+
+        <button className='bg-gradient-to-r from-orange-400 via-pink-600 to-teal-400 text-white px-8 py-3 rounded-lg hover:from-orange-600 hover:to-pink-600 transition-all transform hover:scale-105 mt-4'>
+          Teacher
+        </button>
+        <button className='bg-gradient-to-r from-orange-400 via-pink-600 to-teal-400 text-white px-8 py-3 rounded-lg hover:from-orange-600 hover:to-pink-600 transition-all transform hover:scale-105 mt-4'>
+          Proprietor
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Roles;
